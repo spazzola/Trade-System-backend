60ce079159b5ab5e6df46e271a23f244ed5ed899
Rafal Szczotka <rafalszczotka@wp.pl>
2020-07-22 18:09:54 +0200

    Added error logs to RoleSecurity

--- src/main/java/com/tradesystem/user/RoleSecurity.java
+++ src/main/java/com/tradesystem/user/RoleSecurity.java
@@ -1,6 +1,10 @@
 package com.tradesystem.user;
 
+import com.tradesystem.report.ReportController;
 import com.tradesystem.userdetails.MyUserDetails;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -9,11 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.naming.NoPermissionException;
 import java.util.NoSuchElementException;
 
+@Log4j2
 @Service
 public class RoleSecurity {
 
     private static final String ADMIN_ROLE = "ADMIN";
     private static final String USER_ROLE = "USER";
+
+    private Logger logger = LogManager.getLogger(RoleSecurity.class);
 
     @Autowired
     private UserDao userDao;
@@ -26,6 +33,7 @@ public class RoleSecurity {
                 .orElseThrow(NoSuchElementException::new);
 
         if(!ADMIN_ROLE.equals(user.getRole())) {
+            logger.error("Brak autoryzacji dla: " + user.getLogin());
             throw new PermissionDeniedException();
         }
     }
@@ -36,8 +44,10 @@ public class RoleSecurity {
         final User user = userDao.findByLogin(myUserDetails.getUsername())
                 .orElseThrow(NoSuchElementException::new);
 
-        if(!USER_ROLE.equals(user.getRole())) {
+        if(!USER_ROLE.equals(user.getRole()) && !ADMIN_ROLE.equals(user.getRole())) {
+            logger.error("Brak autoryzacji dla: " + user.getLogin());
             throw new PermissionDeniedException();
         }
     }
+
 }
