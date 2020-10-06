0023c7066e29e378d99f0d06cd6dc5506bb8f3ed
Rafal Szczotka <rafalszczotka@wp.pl>
2020-10-06 14:06:48 +0100

    Added to main class extending from SpringBootServletInitializer and overriding configure method

--- src/main/java/com/tradesystem/TradesystemApplication.java
+++ src/main/java/com/tradesystem/TradesystemApplication.java
@@ -4,14 +4,21 @@ package com.tradesystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TradesystemApplication {
+@EnableAutoConfiguration
+public class TradesystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TradesystemApplication.class, args);
 
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TradesystemApplication.class);
     }
 
 }
